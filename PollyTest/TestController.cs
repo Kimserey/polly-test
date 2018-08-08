76cfa6e2691c51be75618bbf934f842e3ddb34c7
Kimserey <kimserey.lam@groomgy.com>
2018-08-08 21:23:53 +0100

    Added Mvc controller

---
+++ PollyTest/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PollyTest
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("timeout")]
+        public ActionResult GetTimeout()
+        {
+            return Ok();
+        }
+
+        [HttpGet("retry")]
+        public ActionResult GetRetry()
+        {
+            return Ok();
+        }
+
+        [HttpGet("circuit-breaker")]
+        public ActionResult GetCircuitBreaker()
+        {
+            return Ok();
+        }
+    }
+}
